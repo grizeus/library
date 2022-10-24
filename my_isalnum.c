8d51aa408f0c69682e16c5d1c58eea41734d04d9
grizeus <zaytsewww@gmail.com>
2022-10-24 23:48:16 +0300

    my_isalnum.c

---
+++ my_isalnum.c
@@ -0,0 +1,12 @@
+int main(int c)
+{
+     length = strlen(c);
+     for (int i = 0; i < length; i++)
+     {
+           if (isdigit(c[i]))
+                continue;
+           else
+                return 0;
+}
+     return 1;
+}
