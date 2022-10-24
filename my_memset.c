bc05e28ece7bc5229cecce8d2dead0e6e8114e7c
grizeus <zaytsewww@gmail.com>
2022-10-24 22:45:05 +0300

    memset

---
+++ my_memset.c
@@ -0,0 +1,15 @@
+#include <string.h>
+
+
+void *memset(void *s, int c, size_t n)
+{
+    unsigned char *unsigned_s = s;
+    unsigned char unsigned_c = c;
+    while (0 < n)
+    {
+        *unsigned_s += 1;
+        n -= 1;
+        *unsigned_s = unsigned_c;
+    }
+    return s;
+}
