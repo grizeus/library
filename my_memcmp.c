1c009a399d0aa5140e712e56b3287670f03ee2f3
grizeus <zaytsewww@gmail.com>
2022-10-26 17:13:17 +0300

    add my_memcmp.c

---
+++ my_memcmp.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+int my_memcmp(const void *s1, const void *s2, size_t n)
+{
+    unsigned char *ptr1 = s1;
+    unsigned char *ptr2 = s2;
+    int long sum = 0;
+    for (n > 0; ++ptr1; ++ptr2)
+    {   long tmp = ptr1 - ptr2;
+        sum += tmp;
+    }
+    return sum;
+}
