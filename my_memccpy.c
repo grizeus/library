3bc160bf539c297bd9b472190fca3a76f85acd4b
grizeus <zaytsewww@gmail.com>
2022-10-26 15:27:26 +0300

    add my_memccpy.c

---
+++ my_memccpy.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+void *my_memccpy(void *dest, const void *src, int c, size_t n)
+{
+    unsigned char *tmp = *src;
+    unsigned char uc = c;
+    while (*tmp != uc)
+    {   
+        my_memcpy(dest, src, n);
+        return NULL;
+    }
+    if (*tmp == uc)
+        return *tmp;
+}
