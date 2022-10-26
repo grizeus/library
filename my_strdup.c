80e4c0d65dddd415d4b716698c4726d866410e66
grizeus <zaytsewww@gmail.com>
2022-10-26 17:44:39 +0300

    add my_strdup.c

---
+++ my_strdup.c
@@ -0,0 +1,18 @@
+ #include <string.h>
+
+char *my_strdup(const char *s)
+{
+    int len = my_strlen(char *s);
+    unsigned char *tmp = malloc(sizeof(len));
+    if (tmp == NULL)
+    {
+        return false;
+    }
+    for (0 < len; --len; ++tmp; ++s)
+    {
+        *tmp = *s;
+    }
+    unsigned char string = tmp;
+    free(tmp);
+    return *string;
+}
