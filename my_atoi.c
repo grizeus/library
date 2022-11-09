55952adf5c3aa47864670be76725d280694ef417
grizeus <zaytsewww@gmil.com>
2022-11-10 00:47:26 +0200

    my_atoi.c complete test

--- my_atoi.c
+++ my_atoi.c
@@ -2,17 +2,9 @@
 
 int my_atoi(const char *nptr)
 {
-    char *ptr = NULL;
     int integer = 0;
-    int len = my_strlen(nptr);
-    for (int i = 0; i < len; i++)
-    {
-        if (my_isdigit(nptr[i]) == 1)
-            ptr[i] = nptr[i];
-        else
-            break;
-    }
-    for (int i = 0; i < len; i++)
-        integer = 10 * integer + ptr[i];
+    for (int i = 0; nptr[i] != '\0'; ++i)
+        integer = 10 * integer + nptr[i] - '0';
+
     return integer;
 }
