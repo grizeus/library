68e679b4a185e2eb92083c57983908bcb3ba1038
grizeus <zaytsewww@gmail.com>
2022-11-11 12:25:04 +0200

    atoi last test await & isdigit correct

--- my_atoi.c
+++ my_atoi.c
@@ -2,9 +2,27 @@
 
 int my_atoi(const char *nptr)
 {
-    int integer = 0;
-    for (int i = 0; nptr[i] != '\0'; ++i)
-        integer = 10 * integer + nptr[i] - '0';
+    int res = 0, sign = 1;
+    
+    if (*nptr == '\0'){
+        return 0;
+    }
 
-    return integer;
+    if (*nptr == '-'){
+        sign = -1;
+        nptr++;
+    }
+    if (*nptr == '\t'){
+        nptr++;
+    }
+
+    while (*nptr == ' ')
+        nptr++;
+    
+    while (my_isdigit(*nptr) == 1){
+        res = res * 10;
+        res += *nptr - '0';
+        nptr++;      
+    } 
+    return res * sign;
 }
