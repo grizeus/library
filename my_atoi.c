f8c719825e8bb179a86479edc4caa808f558ca51
grizeus <zaytsewww@gmail.com>
2022-11-11 13:02:42 +0200

    try to add lim

--- my_atoi.c
+++ my_atoi.c
@@ -2,7 +2,7 @@
 
 int my_atoi(const char *nptr)
 {
-    int res = 0, sign = 1;
+    int res = 0, sign = 1, INT_MAX = 214783647, INT_MIN = -2147483648;
     
     if (*nptr == '\0'){
         return 0;
@@ -20,6 +20,13 @@ int my_atoi(const char *nptr)
         nptr++;
     
     while (my_isdigit(*nptr) == 1){
+        if (res > INT_MAX / 10)
+        {
+            if (sign == 1)
+                return INT_MAX;
+            else
+                return INT_MIN;
+        }
         res = res * 10;
         res += *nptr - '0';
         nptr++;      
