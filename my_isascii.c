06f6c4ca02e747d756c8f088119689006406003f
grizeus <zaytsewww@gmail.com>
2023-05-23 23:34:38 +0300

    isascii & isprint dox

--- my_isascii.c
+++ my_isascii.c
@@ -1,8 +1,10 @@
 #include "my_library.h"
 
-int my_isascii(int c)
-{
-     if (c >= 0 && c <= 127)
+/// @brief isascii() - checks for an ASCII character
+/// @param c
+/// @return 1 if c is a 7-bit US-ASCII character code, between 0 and hex 0x7F inclusive, 0 otherwise
+int my_isascii(int c) {
+     if (c >= 0x0 && c <= 0x7F)
            return 1;
      return 0;
 }
