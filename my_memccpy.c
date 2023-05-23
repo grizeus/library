ac59e96df2c4f8dc76bab2756299401d75e64200
grizeus <zaytsewww@gmail.com>
2023-05-24 00:18:30 +0300

    memccpy rewrite & dox added

--- my_memccpy.c
+++ my_memccpy.c
@@ -1,10 +1,25 @@
 #include "my_library.h"
 
-void *my_memccpy(void* dest, const void* src, int c, size_t n)
-{
-    void* ptr = my_memchr(src, c, n);
-    if (ptr != NULL)
-        my_memcpy(dest, src, ptr - src + 1);
-    
-    my_memcpy(dest, src, n);
+/// @brief Copies no more than n bytes from memory area src to memory area dest, stopping when the character c is found.
+/// @param dest
+/// @param src
+/// @param c
+/// @param n
+/// @return pointer to the next character in dest after c, or NULL if c was not found in the first n characters of src.
+void* my_memccpy(void* dest, const void* src, int c, size_t n) {
+    unsigned char* dest1 = (unsigned char*) dest;
+    unsigned char* src1 = (unsigned char*) src;
+
+    if (dest1 == NULL || src1 == NULL)
+        return NULL;
+
+    while (n > 0){
+        *dest1 = *src1;
+        if (*src1 == c)
+            return dest1 + 1;
+        dest1++;
+        src1++;
+        n--;
+    }
+    return NULL;
 }
