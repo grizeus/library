3db41a58c9e2dfa51ae8af8cd1828921390b1c16
grizeus <zaytsewww@gmail.com>
2023-05-27 01:22:21 +0300

    strncpy

--- my_memccpy.c
+++ my_memccpy.c
@@ -1,10 +1,10 @@
 #include "my_library.h"
 
 /// @brief Copies no more than n bytes from memory area src to memory area dest, stopping when the character c is found.
-/// @param dest
-/// @param src
-/// @param c
-/// @param n
+/// @param src memory area to be copied from
+/// @param dest memory area to be copied to
+/// @param c character to be searched
+/// @param n maximum number of bytes to be copied
 /// @return pointer to the next character in dest after c, or NULL if c was not found in the first n characters of src.
 void* my_memccpy(void* dest, const void* src, int c, size_t n) {
     unsigned char* dest1 = (unsigned char*) dest;
