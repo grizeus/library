dcf8bfd36b0128d12cb5cff2bdaaf74db99b2d79
grizeus <zaytsewww@gmail.com>
2023-05-26 18:46:54 +0300

    strmap

--- my_strmap.c
+++ my_strmap.c
@@ -1,25 +1,21 @@
 #include "my_library.h"
 
-char* my_strmap(char const *s, char (*f)(char)){
-    if (!s || !f)
-		return (NULL);
+/// @brief Applies the function func to each character of the string passed as argument to create a "fresh" new string (with malloc(3)) resulting from the successive applications of func.
+/// @param str The string to map.
+/// @param func The function to apply to each character of str.
+/// @return The "fresh" string created from the successive applications of func.
+char* my_strmap(char const* str, char (*func)(char)){
+    if (!str || !func)
+		    return NULL;
+
     size_t len = my_strlen(s);
-    char* map_string;
-    if ((map_string = my_strnew(len)) == 0)
-        return NULL;
-    for (size_t i = 0; i < len; i++)
-      map_string[i] = (*f)(s[i]);
+    // allocate memory for the new string
+    char* map_string = my_strnew(len);
+
+    if (map_string != NULL) {
+        for (size_t i = 0; i < len; i++)
+            map_string[i] = (*func)(str[i]);
+    }
     
     return map_string;
 }
-
-
-
-
-
-
-/* 
-Applies the f to each character of the string given as argument to create a "fresh" new string (with malloc(3)) resulting from the successive applications of f.
-Param#1     The string to map.
-Param#2     The function to apply to each character of s.
-Return val  The "fresh" string created from the successive applications of f. */
