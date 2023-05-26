2de15e9bf955df549126cf2eebb14da0a906c4cc
grizeus <zaytsewww@gmail.com>
2023-05-26 16:03:30 +0300

    strchr

--- my_strchr.c
+++ my_strchr.c
@@ -1,13 +1,16 @@
 #include "my_library.h"
 
-char *my_strchr(const char *s, int c)
-{
-    const char *ptr = s;
-    size_t len = my_strlen(s);
-    for (size_t i = 0; i < len; i++)
-    {
-        if (ptr[i] == c)
-            return (void *) ptr;
+/// @brief Locates the first occurrence of c (converted to a char) in the string pointed to by str.
+/// @param str pointer to the string to be scanned
+/// @param c character to be located
+/// @return pointer to the first occurrence of c in the string str, or NULL if the character is not found.
+char* my_strchr(const char* str, int c) {
+    char c_c = (char) c;
+    while (*str != c_c) {
+        if (!*str++)
+            return NULL;
+        str++;
     }
-    return NULL;
+
+    return str;
 }
