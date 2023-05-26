14e3e7eeca097a3f458751f96cd17699b286fe70
grizeus <zaytsewww@gmail.com>
2023-05-26 17:03:06 +0300

    strequ

--- my_strequ.c
+++ my_strequ.c
@@ -1,22 +1,14 @@
 #include "my_library.h"
 
-int my_strequ(char const *s1, char const *s2){
-    if (!s1 || !s2)
-        return 0;
-    size_t len_1 = my_strlen(s1);
-    if (len_1 != my_strlen(s2))
-        return 0;
-    for (size_t i = 0; i < len_1; i++){
-        if (s1[i] != s2[i])
-            return 0;
+/// @brief Lexicographically compare the null-terminated strings s1 and s2.
+/// @param s1 string to be compared
+/// @param s2 string to be compared
+/// @return 1 if the strings are identical, 0 otherwise.
+int my_strequ(char const* s1, char const* s2){
+    while (*s1 && *s2 && (*s1 == *s2)) {
+        s1++;
+        s2++;
     }
-    return 1;
+
+    return (*s1 == '\0' && *s2 == '\0');
 }
-
-
-
-/* Lexicographical comparison between s1 and s2. If the 2 strings are identical function returns 1, 
-or 0 otherwise.
-Param#1     The first string
-Param#2     The second string.
-Return val  1 or 0 according to if the 2 strings are identical or not. */
