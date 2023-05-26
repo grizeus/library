a87ae09b23925031b558beffc5a0afdb53d0537a
grizeus <zaytsewww@gmail.com>
2023-05-26 23:55:05 +0300

    strstr

--- my_strstr.c
+++ my_strstr.c
@@ -1,25 +1,28 @@
 #include "my_library.h"
 
-char *my_strstr(const char *haystack, const char *needle)
-{
-    register char *str, *sub;
+/// @brief Locates the first occurrence substring(needle) in the null-terminated string haystack.
+/// @param haystack string to be scanned
+/// @param needle substring to be searched
+/// @return pointer to the first occurrence of the substring needle in the string haystack, or NULL if the substring is not found.
+char* my_strstr(const char* haystack, const char* needle) {
 
-    sub = (void *) needle;
+    char* sub = needle;
+    // empty string is always a substring
     if (*sub == 0)
-        return (void *) haystack;
+        return haystack;
+
     for ( ; *haystack != 0; haystack++)
     {
         if (*haystack != *sub)
             continue;
-        str = (void *) haystack;
-        while (1) // infinite loop;
-        {
+        char* str = haystack;
+        while (1) { // infinite loop
             if (*sub == 0)
-                return (void *) haystack;
+                return haystack;
             if (*str++ != *sub++)
                 break;
         }   
-        sub = (void *) needle;
+        sub = needle;
     }
     return NULL;
 }
