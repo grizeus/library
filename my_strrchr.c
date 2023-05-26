51c740f2705068dc879753f7964b2886dee58432
grizeus <zaytsewww@gmail.com>
2023-05-27 00:24:58 +0300

    strrchr

--- my_strrchr.c
+++ my_strrchr.c
@@ -1,13 +1,17 @@
 #include "my_library.h"
 
-char *my_strrchr(const char *s, int c)
-{
-    const char *ptr = s;
-    size_t len = my_strlen(s);
-    for (size_t i = 0; len > i; len--)
-    {
-        if (ptr[len] == c)
-            return (void *) ptr;
+/// @brief Locates the last occurrence of c (converted to a char) in the string pointed to by str.
+/// @param str string to be scanned
+/// @param c character to be searched
+/// @return pointer to the last occurrence of the character c in the string str, or NULL if the character is not found.
+char* my_strrchr(const char* str, int c) {
+    const char* last_occurrence = NULL;
+    while (*str != '\0') {
+        if (*str == (char)c)
+            last_occurrence = str;
+        str++;
     }
-    return NULL;
+    if (c == '\0')
+        return (char*)str;
+    return (char*)last_occurrence;
 }
