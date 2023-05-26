251609e5ae258760a5affb1fdbb2d31c64c1d7f2
grizeus <zaytsewww@gmail.com>
2023-05-26 18:02:11 +0300

    strlcat

--- my_strlcat.c
+++ my_strlcat.c
@@ -1,15 +1,20 @@
 #include "my_library.h"
 
-size_t my_strlcat (char *dst, const char *src, size_t size)
-{
-    size_t dest_len = my_strlen(dst);
+/// @brief Appends the string pointed to by src to the end of the string pointed to by dest.
+/// @param dest pointer to the destination array, which should contain a C string, and be large enough to contain the concatenated resulting string
+/// @param src C string to be appended
+/// @param size size of the destination array
+/// @return size of the resulting string dest, which is a C string.
+size_t my_strlcat (char* dst, const char* src, size_t size) {
+    size_t dst_len = my_strlen(dst);
     size_t src_len = my_strlen(src);
-    if ((size - dest_len - 1) > 0)
-    {
-        size_t i;
-        for (i = 0; i < size; i++)
-            dst[dest_len + i] = src[i];
-        dst[dest_len + i] = '\0';
+    // space left in dst
+    size_t append_len = size - dst_len - 1;
+    if (append_len > 0) {
+        size_t i = 0;
+        for (i; i < append_len && src[i] != '\0'; i++)
+            dst[dst_len + i] = src[i];
+        dst[dst_len + i] = '\0';
     }
       
     return dest_len + src_len;
