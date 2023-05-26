4ace128dc9b658a1da532314ec59ae870184705e
grizeus <zaytsewww@gmail.com>
2023-05-26 17:19:54 +0300

    striter

--- my_striter.c
+++ my_striter.c
@@ -1,17 +1,15 @@
 #include "my_library.h"
 
-void my_striter(char *s, void (*f)(char*)){
-    if (s && f){
-        while (*s){
-            (*f)(s);
-            ++s;
+/// @brief Applies the function func to each character of the string passed as argument.
+/// Each character is passed by address to f to be modified if necessary
+/// @param str The string to iterate.
+/// @param func The function to apply to each character of s.
+/// @return none
+void my_striter(char *str, void (*func)(char*)){
+    if (str && func){
+        while (*str){
+            func(str);
+            str++;
         } 
-    }  
+    }
 }
-        
-        
-        
-/* Applies the function f to each character of the string passed as argument. 
-Each character is passed by address ot f to be modified if necessary
-Param#1     The string to iterate.
-Param#2     The function to apply to each character of s. */
