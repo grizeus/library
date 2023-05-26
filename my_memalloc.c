d9bb69d6563e81b49a59a37f80cae433d5777b8d
grizeus <zaytsewww@gmail.com>
2023-05-26 16:51:12 +0300

    strdup

--- my_memalloc.c
+++ my_memalloc.c
@@ -3,9 +3,8 @@
 /// @brief Allocates (with malloc) and retuns a "fresh" memory area.
 /// @param size 
 /// @return ptr to allocated memory area or NULL if allocation fails
-void* my_memalloc(size_t size)
-{
-    void *ptr = malloc(size);
+void* my_memalloc(size_t size) {
+    void* ptr = malloc(size);
     if (ptr == NULL)
         return NULL;
     my_bzero(ptr, size);
--- my_strdup.c
+++ my_strdup.c
@@ -1,9 +1,13 @@
 #include "my_library.h"
 
-char *my_strdup(const char *s)
-{
-    size_t len = my_strlen(s);
-    char *string = my_memalloc(len + 1);
-    my_memcpy(string, s, len);
-    return string;
+/// @brief Allocates sufficient memory for a copy of the string str, does the copy, and returns a pointer to it.
+/// @param str pointer to the string to be copied
+/// @return pointer to the allocated memory area or NULL if insufficient memory was available
+char* my_strdup(const char* str) {
+    size_t len = my_strlen(str);
+    char* new_str = my_memalloc(len + 1);
+    if (new_str == NULL)
+        return NULL;
+    my_memcpy(new_str, str, len);
+    return new_str;
 }
