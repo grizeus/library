1d6a0bb1120262794460aaff808549599ad3a034
grizeus <zaytsewww@gmail.com>
2023-05-26 16:31:42 +0300

    strcmp

--- my_strcmp.c
+++ my_strcmp.c
@@ -1,16 +1,14 @@
 #include "my_library.h"
 
-int my_strcmp(const char *s1, const char *s2)
-{
-    const unsigned char *ptr1 = (const unsigned char *) s1;
-    const unsigned char *ptr2 = (const unsigned char *) s2;
-    unsigned char uc1, uc2;
-    do {
-        uc1 = (unsigned char) *ptr1++;
-        uc2 = (unsigned char) *ptr2++;
-        if (uc1 == '\0')
-	    return uc1 - uc2;
-    }   while (uc1 == uc2);
+/// @brief Compares the string pointed to by s1 to the string pointed to by s2.
+/// @param s1 string to be compared
+/// @param s2 string to be compared
+/// @return positive if s1 is lexicographically greater than s2, negative if s1 is less than s2, 0 if s1 is equal to s2.
+int my_strcmp(const char *s1, const char *s2) {
+    while (*s1 && *s1 == *s2) {
+        s1++;
+        s2++;
+    }
 
-    return uc1 - uc2;    
+    return *(unsigned char *)s1 - *(unsigned char *)s2;
 }
